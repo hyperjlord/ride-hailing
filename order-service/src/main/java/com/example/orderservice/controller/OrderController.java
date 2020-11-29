886dd25ac247bc12296c825eef93a407fdbce63b
1853081 <49673560+1853081@users.noreply.github.com>
2020-11-29 14:28:06 +0800

    整合了swagger2，以后可以自动生成api在线文档了

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.example.orderservice.controller;
 import com.example.orderservice.dao.OrderMapper;
 import com.example.orderservice.pojo.Order;
 import com.example.orderservice.service.OrderService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,23 +23,29 @@ public class OrderController {
     @Autowired
     public OrderService orderService;
     //@GetMapping(value="/hello",produces = "application/json;charset=UTF-8")
+    @ApiOperation(value = "hello world",notes = "测试用的helloworld")
     @GetMapping("/hello")
     public Test helloworld(){
         Test test=new Test();
         return test;
     }
+    @ApiOperation(value = "查询所有订单",notes="返回数据库中所有的订单信息")
     @GetMapping("/selectall")
     public List<Order> selectall(){
         return orderService.selectAll();
     }
+
+    @ApiOperation(value = "根据用户id返回订单",notes = "根据用户id返回订单")
     @GetMapping("/selectbyid")
-    public Order selectById(@RequestParam("user_id") String user_id){
+    public Order selectById(@ApiParam(value="用户id") @RequestParam("user_id") String user_id){
         return orderService.selectById(user_id);
     }
 
     //返回乘客目的地与司机的目的地最近的订单，按距离从小到大排序
+    @ApiOperation(value = "查询乘客目的地与司机的目的地最近的订单",notes = "返回乘客目的地与司机的目的地最近的订单，按距离从小到大排序")
     @GetMapping("/getmatchorders")
-    public List<Order> getMatchOrders(@RequestParam("lon") Double longitude,@RequestParam("lat") Double latitude){
+    public List<Order> getMatchOrders(@ApiParam(value ="司机所在的经度" ) @RequestParam("lon") Double longitude,
+                                      @ApiParam(value ="司机所在的纬度" ) @RequestParam("lat") Double latitude){
         return  orderService.getMatchOrders(longitude,latitude);
     }
 
