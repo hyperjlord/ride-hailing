8f769a53920534bbff0a99fa202f8a5f9900a020
Cupid0320 <49679591+Cupid0320@users.noreply.github.com>
2020-12-26 21:35:11 +0800

    add some controllers

--- account-service/src/main/java/top/wx/service/Userservice.java
+++ account-service/src/main/java/top/wx/service/Userservice.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import top.wx.Mapper.UserMapper;
 import top.wx.pojo.Passenger;
+import top.wx.pojo.Driver;
 
 @Service
 public class Userservice {
@@ -19,12 +20,24 @@ public class Userservice {
 		return user == null ? false : true;
 	}
 
+	public boolean findDriverIdIsExist(String driverId) {
+		Driver driver=userMapper.findDriverIdIsExist(driverId);
+		return driver == null ? false : true;
+	}
 	public void saveUser(Passenger user) {
 		//user.setUserId(UUID.randomUUID().toString());
 		userMapper.saveUser(user);
 	}
 
+	public void saveDriver(Driver driver) {
+		userMapper.saveDriver(driver);
+	}
+
 	public Passenger queryUserForLogin(String userId, String password) {
 		return userMapper.queryUserForLogin(userId,password);
+	}
+
+	public Driver queryDriverForLogin(String driverId, String password) {
+		return userMapper.queryDriverForLogin(driverId,password);
 	}
 }
