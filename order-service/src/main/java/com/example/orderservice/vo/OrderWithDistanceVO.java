cef7529f23eb42560dba531f78cf558b8fb19f52
1853081 <49673560+1853081@users.noreply.github.com>
2021-01-03 18:43:24 +0800

    修改了匹配订单的返回样式

--- order-service/src/main/java/com/example/orderservice/vo/OrderWithDistanceVO.java
+++ order-service/src/main/java/com/example/orderservice/vo/OrderWithDistanceVO.java
@@ -1,5 +1,8 @@
 package com.example.orderservice.vo;
 
+import com.example.orderservice.dto.DriverDetailDto;
+import com.example.orderservice.pojo.Comment;
+import com.example.orderservice.pojo.Passenger;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
@@ -9,7 +12,8 @@ public class OrderWithDistanceVO {
     public String order_id;
     public int type;
     public int state;
-    public String user_id;
+    //public String user_id;
+    public Passenger passenger;
     public int passenger_num;
     public Double price;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
@@ -20,9 +24,11 @@ public class OrderWithDistanceVO {
     public Double from_lon;
     public Double to_lat;
     public Double to_lon;
-    public String driver_id;
+    //public String driver_id;
+    public DriverDetailDto driver;
     public String description;
     public Double distance;
+    public Comment comment;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
     public Date taken_time;
 }
