27c63a2f8669f3200a9752c730b8e586150ca81d
1853081 <49673560+1853081@users.noreply.github.com>
2021-05-26 12:50:20 +0800

    small change

--- emergency-service/src/main/java/com/soa/emergencyservice/dao/ContactDao.java
+++ emergency-service/src/main/java/com/soa/emergencyservice/dao/ContactDao.java
@@ -10,10 +10,10 @@ import java.util.List;
 
 @Mapper
 public interface ContactDao {
-    @Select("SELECT * FROM soadb.order WHERE order_id = #{order_id}")
+    @Select("SELECT * FROM ridehailing.order WHERE order_id = #{order_id}")
     Order findOrderByOrderId(String order_id);
-    @Select("SELECT * FROM soadb.driver NATURAL JOIN soadb.car WHERE driver_id = #{driver_id} ")
+    @Select("SELECT * FROM ridehailing.driver NATURAL JOIN ridehailing.car WHERE driver_id = #{driver_id} ")
     DriverDetailVo findDriverDetailByDriverId(String driver_id);
-    @Select("SELECT * FROM soadb.contact WHERE user_id = #{user_id}")
+    @Select("SELECT * FROM ridehailing.contact WHERE user_id = #{user_id}")
     List<Contact> findContactByUserId(String user_id);
 }
